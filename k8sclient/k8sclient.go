3245ed85778ae8469fd43112ba045004bc1d4e5d
Tomofumi Hayashi <tohayash@redhat.com>
2018-04-23 15:48:04 +0900

    Fix error type for NoK8sNetworkError
    This fix changes type type for NoK8sNetworkError due to error handling, in #45.
    Currently NoK8sNetworkError is not pointer type, just a string, hence we never
    get *NoK8sNetworkError. This fix changes it based @One-ders patch.

--- k8sclient/k8sclient.go
+++ k8sclient/k8sclient.go
@@ -30,9 +30,11 @@ import (
 )
 
 // NoK8sNetworkError indicates error, no network in kubernetes
-type NoK8sNetworkError string
+type NoK8sNetworkError struct {
+	message string
+}
 
-func (e NoK8sNetworkError) Error() string { return string(e) }
+func (e *NoK8sNetworkError) Error() string { return string(e.message) }
 
 func createK8sClient(kubeconfig string) (*kubernetes.Clientset, error) {
 
@@ -258,7 +260,7 @@ func GetK8sNetwork(args *skel.CmdArgs, kubeconfig string) ([]map[string]interfac
 	}
 
 	if len(netAnnot) == 0 {
-		return podNet, NoK8sNetworkError("no kubernetes network found")
+		return podNet, &NoK8sNetworkError{"no kubernetes network found"}
 	}
 
 	netObjs, err := parsePodNetworkObject(netAnnot)
