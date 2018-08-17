86af6ab69fe2491ef1e33f31f86f7c0f122dae19
Kuralamudhan Ramakrishnan <kuralamudhan.ramakrishnan@intel.com>
2018-08-17 15:52:57 +0100

    Update test.sh with coveralls job inclusion

--- test.sh
+++ test.sh
@@ -13,5 +13,5 @@ export GO15VENDOREXPERIMENT=1
 export GOBIN=${PWD}/bin
 export GOPATH=${PWD}/gopath
 
-bash -c "umask 0; cd ${GOPATH}/src/${REPO_PATH}; PATH=${GOROOT}/bin:$(pwd)/bin:${PATH} go test ./..."
+bash -c "umask 0; cd ${GOPATH}/src/${REPO_PATH}; PATH=${GOROOT}/bin:$(pwd)/bin:${PATH} go test -v -covermode=count -coverprofile=coverage.out ./..."
 
