b6153163426b4daa21a968216943bf5116e2a6bd
Tomofumi Hayashi <tohayash@redhat.com>
2019-06-07 18:36:14 +0900

    Migrate go modules from glide

--- test.sh
+++ test.sh
@@ -1,17 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-ORG_PATH="github.com/intel"
-REPO_PATH="${ORG_PATH}/multus-cni"
+export GO111MODULE=on
 
-if [ ! -h gopath/src/${REPO_PATH} ]; then
-	mkdir -p gopath/src/${ORG_PATH}
-	ln -s ../../../.. gopath/src/${REPO_PATH} || exit 255
-fi
-
-export GO15VENDOREXPERIMENT=1
-export GOBIN=${PWD}/bin
-export GOPATH=${PWD}/gopath
-
-bash -c "umask 0; cd ${GOPATH}/src/${REPO_PATH}; PATH=${GOROOT}/bin:$(pwd)/bin:${PATH} go test -v -covermode=count -coverprofile=coverage.out ./..."
+bash -c "umask 0; go test -v -covermode=count -coverprofile=coverage.out ./..."
 
