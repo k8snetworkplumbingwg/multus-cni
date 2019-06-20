715247071f98ee6fe62f8c8ce6b6921b6b770d65
Nikhil <simha.nikhil@gmail.com>
2019-06-20 13:11:49 -0400

    Improved coverage of checkpoint.go to 96.4%

--- checkpoint/checkpoint_test.go
+++ checkpoint/checkpoint_test.go
@@ -105,6 +105,21 @@ var _ = Describe("Kubelet checkpoint data read operations", func() {
 			resourceMap = rmap
 		})
 
+		// It("should return a ResourceMap instance", func() {
+		// 	podUID := k8sTypes.UID("970a395d-bb3b-11e8-89df-408d5c537d23")
+		// 	fakePod := &v1.Pod{
+		// 		ObjectMeta: metav1.ObjectMeta{
+		// 			Name:      "fakePod",
+		// 			Namespace: "podNamespace",
+		// 			UID:       podUID,
+		// 		},
+		// 	}
+		// 	rmap, err := cp.GetPodResourceMap(fakePod)
+		// 	Expect(err).NotTo(HaveOccurred())
+		// 	Expect(rmap).NotTo(BeEmpty())
+		// 	resourceMap = rmap
+		// })
+
 		It("resourceMap should have value for \"intel.com/sriov_net_A\"", func() {
 			rInfo, ok := resourceMap[resourceAnnot]
 			Expect(ok).To(BeTrue())
@@ -192,7 +207,14 @@ var _ = Describe("Kubelet checkpoint data read operations", func() {
 			rmap, err := cp.GetPodResourceMap(fakePod)
 			Expect(err).To(HaveOccurred())
 			Expect(rmap).To(BeEmpty())
+			// resourceMap = rmap
 		})
+	})
+
+	It("Check public GetCheckpoint for basic functionality", func() {
+		cp, err := GetCheckpoint()
+		Expect(err).NotTo(HaveOccurred())
+		Expect(cp).To(Equal(&checkpoint{fileName: checkPointfile}))
 	})
 })
 
