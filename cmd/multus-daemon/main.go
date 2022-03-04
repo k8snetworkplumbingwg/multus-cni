804ab0d685a61ddc208709275b85d4bf28c17e7f
Cyclinder <qifeng.guo@daocloud.io>
2022-03-04 15:01:21 +0800

    fix the usage of flag "overrideNetworkName" (#805)

--- cmd/multus-daemon/main.go
+++ cmd/multus-daemon/main.go
@@ -89,7 +89,7 @@ func main() {
 	additionalBinDir := flag.String(multusAdditionalBinDirVarName, defaultMultusAdditionalBinDir, "Additional binary directory to specify in the configurations. Used only with --multus-conf-file=auto.")
 	readinessIndicator := flag.String(multusReadinessIndicatorFile, defaultMultusReadinessIndicatorFile, "Which file should be used as the readiness indicator. Used only with --multus-conf-file=auto.")
 	multusKubeconfig := flag.String(multusKubeconfigPath, defaultMultusKubeconfigPath, "The path to the kubeconfig")
-	overrideNetworkName := flag.Bool("override-network-name", false, "Used when ")
+	overrideNetworkName := flag.Bool("override-network-name", false, "Used when we need overrides the name of the multus configuration with the name of the delegated primary CNI")
 	multusBinDir := flag.String(multusBinDirVarName, defaultMultusBinDir, "The directory where the CNI plugin binaries are available")
 	multusCniDir := flag.String(multusCNIDirVarName, defaultMultusCNIDir, "The directory where the multus CNI cache is located")
 
