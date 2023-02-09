e59e2995d00f12420c24eceb54afc6941e49edc8
yanggang <gang.yang@daocloud.io>
2023-02-09 08:58:21 +0800

    delete GO111MODULE=off case in test-go.sh
    Signed-off-by: yanggang <gang.yang@daocloud.io>

--- hack/test-go.sh
+++ hack/test-go.sh
@@ -1,23 +1,4 @@
 #!/usr/bin/env bash
 set -e
 
-# this if... will be removed when gomodules goes default
-if [ "$GO111MODULE" == "off" ]; then
-	echo "Warning: this will be deprecated in near future so please use go modules!"
-
-	ORG_PATH="gopkg.in/k8snetworkplumbingwg"
-	REPO_PATH="${ORG_PATH}/multus-cni"
-
-	if [ ! -h gopath/src/${REPO_PATH} ]; then
-		mkdir -p gopath/src/${ORG_PATH}
-		ln -s ../../../.. gopath/src/${REPO_PATH} || exit 255
-	fi
-
-	export GO15VENDOREXPERIMENT=1
-	export GOBIN=${PWD}/bin
-	export GOPATH=${PWD}/gopath
-	bash -c "umask 0; cd ${GOPATH}/src/${REPO_PATH}; PATH=${GOROOT}/bin:$(pwd)/bin:${PATH} go test -v -covermode=count -coverprofile=coverage.out ./..."
-else
-	# test with go modules
-	bash -c "umask 0; go test -v -covermode=count -coverprofile=coverage.out ./..."
-fi
+bash -c "umask 0; go test -v -covermode=count -coverprofile=coverage.out ./..."
