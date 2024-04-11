6a15e99dd3211a5010b89e047cef48c5aac50316
Vasilis Remmas <vremmas@nvidia.com>
2024-04-11 19:21:36 +0200

    Add test to ensure pods on control plane can access Kube API server
    This test is added to showcase primary network not working as expected
    on Kind control plane nodes with thick plugin installed.
    
    Signed-off-by: Vasilis Remmas <vremmas@nvidia.com>

--- e2e/test-simple-macvlan1.sh
+++ e2e/test-simple-macvlan1.sh
@@ -38,5 +38,21 @@ if [ $ipaddr != "10.1.1.12" ]; then
 	echo "macvlan1-worker2 IP address is different: ${ipaddr}"
 fi
 
+echo "check eventual connectivity of macvlan1-control-plane Pod to the Kubernetes API server"
+for i in `seq 1 10`;
+do
+    if [ $(kubectl exec macvlan1-control-plane -- nc -zvw1 kubernetes 443 >/dev/null && echo $? || echo $?) -eq 0 ]; then
+        echo "macvlan1-control-plane reached the Kubernetes API server"
+        break
+    fi
+
+    if [ $i -eq 10 ]; then
+        echo "macvlan1-control-plane couldn't connect to the Kubernetes API server"
+        exit 1
+    fi
+
+    sleep 1
+done
+
 echo "cleanup resources"
 kubectl delete -f yamls/simple-macvlan1.yml
