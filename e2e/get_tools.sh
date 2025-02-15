aa3283b6531a6a953c3914ae8ff35e136f823c70
Davanum Srinivas <davanum@gmail.com>
2025-02-14 20:21:00 -0500

    update to latest kind and stable k8s release
    Signed-off-by: Davanum Srinivas <davanum@gmail.com>

--- e2e/get_tools.sh
+++ e2e/get_tools.sh
@@ -5,9 +5,9 @@ if [ ! -d bin ]; then
 	mkdir bin
 fi
 
-curl -Lo ./bin/kind "https://github.com/kubernetes-sigs/kind/releases/download/v0.22.0/kind-$(uname)-amd64"
+curl -Lo ./bin/kind "https://github.com/kubernetes-sigs/kind/releases/download/v0.24.0/kind-$(uname)-amd64"
 chmod +x ./bin/kind
-curl -Lo ./bin/kubectl https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+curl -Lo ./bin/kubectl https://storage.googleapis.com/kubernetes-release/release/`curl -Ls https://dl.k8s.io/release/stable.txt`/bin/linux/amd64/kubectl
 chmod +x ./bin/kubectl
 curl -Lo ./bin/koko https://github.com/redhat-nfvpe/koko/releases/download/v0.83/koko_0.83_linux_amd64
 chmod +x ./bin/koko
