k8snetworkplumbingwg/multus-cni

commit f18d96b648559d1b67d21eb8b772df51c7a493e7

update to go 1.23 and latest k8s version (1.32.5)
Signed-off-by: Muhammad Adil Ghaffar <muhammad.adil.ghaffar@est.tech>

pkg/netutils/netutils.go
@@ -25,6 +25,7 @@ import (
 	"github.com/containernetworking/cni/libcni"
 	"github.com/containernetworking/plugins/pkg/ns"
 	"github.com/vishvananda/netlink"
+	"golang.org/x/sys/unix"
 	"gopkg.in/k8snetworkplumbingwg/multus-cni.v4/pkg/logging"
 )
 
@@ -82,7 +83,12 @@ func SetDefaultGW(netnsPath string, ifName string, gateways []net.IP) error {
 			// Perform the creation of the default route....
 			err = netlink.RouteAdd(&newDefaultRoute)
 			if err != nil {
-				logging.Errorf("SetDefaultGW: Error adding route: %v", err)
+				if os.IsExist(err) || err == unix.EEXIST {
+					logging.Debugf("SetDefaultGW: Route already exists, ignoring: %v", err)
+					err = nil
+				} else {
+					logging.Errorf("SetDefaultGW: Error adding route: %v", err)
+				}
 			}
 		}
 		return err

commit 26340cd64bf7cf4e051ec8d29e0f8c479c9101d9

Fix default route replacement
Multus was failing to detect existing Default routes in container and
remove them before replacement, which causes later adding of default
route to fail with EEXIST, which is ignored and not detected as a error.

This leads to container keeping existing default route in case of
chained CNIs instead of getting new default route from Multus.

Fixes regression introduced by f18d96b648559d1b67d21eb8b772df51c7a493e7

There was a breaking change in that bump:
https://github.com/vishvananda/netlink/commit/acdc658b8613655ddb69f978e9fb4cf413e2b830
route.Dst is a zero IPNet instead of nil

Check for default route using isIPNetZero adopted from
github.com/containernetworking/plugins/pkg/ip.IsIPNetZero. Avoid
importing ip.IsIPNetZero to avoid brining more dependencies.

Signed-off-by: Viktor Oreshkin <imselfish@stek29.rocks>

pkg/netutils/netutils.go
@@ -30,6 +30,16 @@ import (
 	"gopkg.in/k8snetworkplumbingwg/multus-cni.v4/pkg/logging"
 )
 
+func isIPNetZero(ipnet *net.IPNet) bool {
+	if ipnet == nil {
+		return true
+	}
+	if ones, _ := ipnet.Mask.Size(); ones != 0 {
+		return false
+	}
+	return ipnet.IP.Equal(net.IPv4zero) || ipnet.IP.Equal(net.IPv6zero)
+}
+
 // DeleteDefaultGW removes the default gateway from marked interfaces.
 func DeleteDefaultGW(netnsPath string, ifName string) error {
 	netns, err := ns.GetNS(netnsPath)
@@ -43,7 +53,7 @@ func DeleteDefaultGW(netnsPath string, ifName string) error {
 		link, _ := netlink.LinkByName(ifName)
 		routes, _ := netlink.RouteList(link, netlink.FAMILY_ALL)
 		for _, nlroute := range routes {
-			if nlroute.Dst == nil {
+			if isIPNetZero(nlroute.Dst) {
 				err = netlink.RouteDel(&nlroute)
 			}
 		}

pkg/netutils/netutils_test.go
@@ -40,6 +40,17 @@ func TestNetutils(t *testing.T) {
 	RunSpecs(t, "netutils")
 }
 
+var _ = Describe("default route detection", func() {
+	DescribeTable("detects default routes",
+		func(dst *net.IPNet) {
+			Expect(isIPNetZero(dst)).To(BeTrue())
+		},
+		Entry("nil destination", nil),
+		Entry("IPv4 zero prefix", &net.IPNet{IP: net.IPv4zero, Mask: net.CIDRMask(0, 32)}),
+		Entry("IPv6 zero prefix", &net.IPNet{IP: net.IPv6zero, Mask: net.CIDRMask(0, 128)}),
+	)
+})
+
 // helper function
 func testAddRoute(link netlink.Link, ip net.IP, mask net.IPMask, gw net.IP) error {
 	dst := &net.IPNet{
@@ -232,6 +243,18 @@ var _ = Describe("netutil netlink function testing", func() {
 				defer GinkgoRecover()
 
 				Expect(DeleteDefaultGW(args.Netns, IFNAME)).Should(Succeed())
+				return nil
+			})).Should(Succeed())
+
+			Expect(targetNS.Do(func(ns.NetNS) error {
+				defer GinkgoRecover()
+				link, err := netlink.LinkByName(IFNAME)
+				Expect(err).NotTo(HaveOccurred())
+				routes, err := netlink.RouteList(link, netlink.FAMILY_ALL)
+				Expect(err).NotTo(HaveOccurred())
+				for _, route := range routes {
+					Expect(isIPNetZero(route.Dst)).To(BeFalse())
+				}
 				return nil
 			})).Should(Succeed())
 		})